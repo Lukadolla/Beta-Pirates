459fac814142dcbdbad9c8f2a0af81fb697b4996
Dáire Fagan <dairefagan@gmail.com>
2021-03-19 13:20:23 +0000

    restructure project - part 2!

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,32 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Stage mainStage = new Stage();
+        mainStage.setMaximized(true);
+        mainPage(mainStage);
+    }
+    
+    public void mainPage(Stage mainStage) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/test.fxml"));
+        Scene mainScene = new Scene(root);
+        mainStage.setTitle("Excelsior");
+        mainStage.setScene(mainScene);
+        mainStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
