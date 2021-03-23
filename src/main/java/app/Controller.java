6b68c3e42f549b8da533e35729588f61dc91eed4
Lukasz Filanowski <44058072+Lukadolla@users.noreply.github.com>
2021-03-23 13:47:46 +0000

    Start on image insertion

--- src/main/java/app/Controller.java
+++ src/main/java/app/Controller.java
@@ -1,5 +1,38 @@
 package app;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.image.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Controller {
 
+    @FXML
+    private ImageView bottomLeftIV;
+
+    @FXML
+    private void insertCharacterLeft(ActionEvent event) throws IOException{
+        event.consume();
+        loadImages();
+    }
+
+    @FXML
+    public void loadImages() throws IOException{
+        Image image = new Image(selectImage());
+        bottomLeftIV.setImage(image);
+    }
+
+    @FXML
+    public String selectImage() throws IOException {
+       String output = "";
+       Runtime.getRuntime().exec("explorer /select,../images/characters");
+
+
+       return output;
+    }
 }
