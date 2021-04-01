dabc3be9efb0ac904d058c0a63e7f225f057585b
Dáire Fagan <dairefagan@gmail.com>
2021-04-01 17:25:57 +0100

    add class for list of character images

---
+++ src/main/java/app/CharacterList.java
@@ -0,0 +1,45 @@
+package app;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.image.Image;
+
+public class CharacterList {
+
+  private List<Image> characterImages;
+
+  public CharacterList() {
+    characterImages = new ArrayList<>();
+  }
+
+
+  // investigate replacing directory parameter with something like:
+  // URL imageUrl = getClass().getResource("/images/characters");
+
+  public void loadCharacterImages(File directory) throws MalformedURLException {
+    File[] images = directory.listFiles();
+
+    if (characterImages != null){
+      for (File image : images ) {
+        try {
+          this.addImage(new Image(image.toURI().toURL().toExternalForm(), 100, 100, true, true) );
+        } catch (MalformedURLException e) {
+          e.printStackTrace();
+        }
+      }
+    } else System.out.println("Directory error");
+
+  }
+
+  private void addImage(Image image) {
+    characterImages.add(image);
+  }
+
+  public List<Image> getCharacterImages() {
+    return characterImages;
+  }
+
+}
