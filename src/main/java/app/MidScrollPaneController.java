a87c4bfd0aa6b9c7e110dde228387085a04b231d
Dáire Fagan <dairefagan@gmail.com>
2021-04-22 15:47:14 +0100

    Rename mainController and add buttonsContoller reference.

--- src/main/java/app/MidScrollPaneController.java
+++ src/main/java/app/MidScrollPaneController.java
@@ -1,7 +1,6 @@
 package app;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -11,7 +10,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static javafx.geometry.Pos.CENTER;
 
@@ -20,10 +18,10 @@ public class MidScrollPaneController {
     private ImageLists imageLists = new ImageLists();
     private List<Image> characterImages;  //List of character Images
     private List<Image> backgroundImages;  //List of backgrounds
-    private TestController cont;
+    private TestController mainController;
 
-    public MidScrollPaneController(TestController cont) {
-        this.cont = cont;
+    public MidScrollPaneController(TestController mainController) {
+        this.mainController = mainController;
     }
 
     private void loadCharacterImages() throws IOException {  //Method that loads character images from the CharacterList class and displays them in the middle panel
@@ -43,13 +41,13 @@ public class MidScrollPaneController {
             region.setVisible(true);
             region.setStyle("-fx-border-color: #bbc4c4");
             region.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-                cont.insertCharacter(characterImages.get(finalSelectedImage));
+                mainController.getButtonsController().insertCharacter(characterImages.get(finalSelectedImage));
                 event.consume();
             });
 
             AnchorPane characterAnchorPane = setUpMiddlePanel(selectedImage, imageview, region, "characterHbox");
 
-            cont.charactersGridPane.add(characterAnchorPane,columnIndex,rowIndex);
+            mainController.charactersGridPane.add(characterAnchorPane,columnIndex,rowIndex);
 
             columnIndex = (columnIndex == 0) ? 1 : 0;
         }
@@ -57,9 +55,9 @@ public class MidScrollPaneController {
 
     public void addCharacterPane() throws IOException {
 
-        swapMiddlePanel(cont.backgroundGridPane, cont.charactersGridPane);
-        cont.midScrollPane.setVvalue(0);
-        cont.midScrollPane.setVisible(true);
+        swapMiddlePanel(mainController.backgroundGridPane, mainController.charactersGridPane);
+        mainController.midScrollPane.setVvalue(0);
+        mainController.midScrollPane.setVisible(true);
 
         if(characterImages == null){
             loadCharacterImages();
@@ -67,9 +65,9 @@ public class MidScrollPaneController {
     }
 
     public void addBackgroundPane() throws IOException {
-        swapMiddlePanel(cont.charactersGridPane, cont.backgroundGridPane);
-        cont.midScrollPane.setVvalue(0);
-        cont.midScrollPane.setVisible(true);
+        swapMiddlePanel(mainController.charactersGridPane, mainController.backgroundGridPane);
+        mainController.midScrollPane.setVvalue(0);
+        mainController.midScrollPane.setVisible(true);
 
         if(backgroundImages == null){
             loadBackgroundImages();
@@ -121,19 +119,19 @@ public class MidScrollPaneController {
             region.setStyle("-fx-border-color: #bbc4c4");
 
             region.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-                cont.insertBackground(backgroundImages.get(finalSelectedImage));
+                mainController.insertBackground(backgroundImages.get(finalSelectedImage));
                 event.consume();
             });
 
             AnchorPane characterAnchorPane = setUpMiddlePanel(selectedImage, imageview, region, "backgroundHbox");
 
-            cont.backgroundGridPane.add(characterAnchorPane,columnIndex,rowIndex);
+            mainController.backgroundGridPane.add(characterAnchorPane,columnIndex,rowIndex);
 
             columnIndex = (columnIndex == 0) ? 1 : 0;
         }
     }
 
-    public GridPane getCharactersGridPane() { return cont.charactersGridPane; }
+    public GridPane getCharactersGridPane() { return mainController.charactersGridPane; }
 
-    public GridPane getBackgroundGridPane() { return cont.backgroundGridPane; }
+    public GridPane getBackgroundGridPane() { return mainController.backgroundGridPane; }
 }
