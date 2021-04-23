5e9d15348389728eff7c1484b7e9e6d91974236e
Dáire Fagan <dairefagan@gmail.com>
2021-04-23 13:28:36 +0100

    actually push the buttonsController, not just main controller edits.

---
+++ src/main/java/app/ButtonsController.java
@@ -0,0 +1,212 @@
+package app;
+
+import java.io.IOException;
+import java.net.URL;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
+public class ButtonsController {
+
+  private TestController mainController;
+
+
+  public ButtonsController(TestController controller) { this.mainController = controller; }
+
+  @FXML
+  void enableToolTips(){ //Method to enable tool tips when mouse is hovered over the buttons
+    mainController.addCharacterLeftButton.setTooltip(
+        new Tooltip("Add a character to the left side")
+    );
+
+    mainController.addCharacterRightButton.setTooltip(
+        new Tooltip("Add a character to the right side")
+    );
+
+    mainController.speechBubbleButton.setTooltip(
+        new Tooltip("Add speech bubble")
+    );
+
+    mainController.thoughtBubbleButton.setTooltip(
+        new Tooltip("Add thought bubble")
+    );
+
+    mainController.rotateCharacterButton.setTooltip(
+        new Tooltip("Rotate character")
+    );
+
+    mainController.changeGenderButton.setTooltip(
+        new Tooltip("Change gender of character")
+    );
+
+    mainController.deleteCharacterButton.setTooltip(
+        new Tooltip("Delete character")
+    );
+
+    mainController.backgroundButton.setTooltip(
+        new Tooltip("Add background")
+    );
+
+    mainController.hairColourPicker.setTooltip(
+        new Tooltip("Change hair colour")
+    );
+
+    mainController.bodyColourPicker.setTooltip(
+        new Tooltip("Change body colour")
+    );
+  }
+
+  @FXML
+  void addCharacterLeft() throws IOException { //Method called when button is pressed to add a character into the left panel
+
+    mainController.getMidScrollPaneController().addCharacterPane();
+
+    if(mainController.bottomLeftIV.getImage() == null){
+      switchButtonState(false);
+    } else {
+      switchButtonState(true);
+    }
+
+    mainController.setBorder(mainController.bottomLeftBorder);
+    mainController.comicSelection = mainController.bottomLeftIV;
+    mainController.comicCharacterSelection = mainController.bottomLeftIV;
+  }
+
+  @FXML
+  void addCharacterRight() throws IOException { //Method called when button is pressed to add a character into the right panel
+
+    mainController.getMidScrollPaneController().addCharacterPane();
+
+    if(mainController.bottomRightIV.getImage() == null){
+      switchButtonState(false);
+    } else {
+      switchButtonState(true);
+    }
+
+    mainController.setBorder(mainController.bottomRightBorder);
+    mainController.comicSelection = mainController.bottomRightIV;
+    mainController.comicCharacterSelection = mainController.bottomRightIV;
+  }
+
+
+  public void insertCharacter(Image selectedImage) {
+    if (mainController.comicCharacterSelection == mainController.bottomLeftIV) {
+      insertLeftCharacter(selectedImage);
+    } else if (mainController.comicCharacterSelection == mainController.bottomRightIV) {
+      insertRightCharacter(selectedImage);
+    }
+  }
+
+  @FXML
+  public void insertRightCharacter(Image selectedImage){  //Method that inserts a character into the right panel and adds character data to the Comic class
+    mainController.comic.setRightCharacter(new Character(selectedImage, 1));
+    mainController.comic.setSelected(mainController.comic.getRightCharacter());
+    mainController.bottomRightIV.setImage(mainController.comic.getRightCharacter().getImage());
+    mainController.bottomRightIV.setScaleX(-1);
+    mainController.bottomRightIV.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+      mainController.setBorder(mainController.bottomRightBorder);
+      mainController.comicSelection = mainController.bottomRightIV;
+      mainController.comicCharacterSelection = mainController.bottomRightIV;
+      mainController.comic.setSelected(mainController.comic.getRightCharacter());
+      switchButtonState(true);
+      event.consume();
+    });
+
+    switchButtonState(true);
+    mainController.removeHairAA();
+    mainController.clearBackground();
+    mainController.removeAAPixels();
+  }
+
+  @FXML
+  public void insertLeftCharacter(Image selectedImage){ //Method that inserts a character into the left panel and adds character data to the Comic class
+    mainController.comic.setLeftCharacter(new Character(selectedImage, 1));
+    mainController.comic.setSelected(mainController.comic.getLeftCharacter());
+    mainController.bottomLeftIV.setImage(mainController.comic.getLeftCharacter().getImage());
+    mainController.bottomLeftIV.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+      mainController.setBorder(mainController.bottomLeftBorder);
+      mainController.comicSelection = mainController.bottomLeftIV;
+      mainController.comicCharacterSelection = mainController.bottomLeftIV;
+      mainController.comic.setSelected(mainController.comic.getLeftCharacter());
+      switchButtonState(true);
+      event.consume();
+    });
+    switchButtonState(true);
+    mainController.removeHairAA();
+    mainController.clearBackground();
+    mainController.removeAAPixels();
+  }
+
+  void switchButtonState(boolean areEnabled){
+    mainController.rotateCharacterButton.setDisable(!areEnabled);
+    mainController.changeGenderButton.setDisable(!areEnabled);
+    mainController.bodyColourPicker.setDisable(!areEnabled);
+    mainController.hairColourPicker.setDisable(!areEnabled);
+    mainController.speechBubbleButton.setDisable(!areEnabled);
+    mainController.thoughtBubbleButton.setDisable(!areEnabled);
+    mainController.deleteCharacterButton.setDisable(!areEnabled);
+  }
+
+  @FXML
+  public void changeGender() { //Method called when user presses the change gender button
+
+    if(mainController.comic.getSelected().getGender().equals("female")){
+      mainController.setMale();
+    }
+    else{
+      mainController.setFemale();
+    }
+  }
+
+  @FXML
+  void addSpeechBubble(){ //Method called when user presses the speech bubble button
+
+    URL url = getClass().getResource("/images/buttons/speech.png");
+    String currentPath = url.toString();
+
+    ImageView imageView = new ImageView(currentPath);
+
+    mainController.insertBubble(imageView);
+  }
+
+
+  @FXML
+  void addThoughtBubble(){ //Method called when user presses the thought bubble button
+
+    URL url = getClass().getResource("/images/buttons/thought.PNG");
+    String currentPath = url.toString();
+
+    ImageView imageView = new ImageView(currentPath);
+
+    mainController.insertBubble(imageView);
+  }
+
+  @FXML
+  void deleteCharacter() {  //Method called when the user presses the delete button which removes characters and text from the selected half of the comic
+    if(mainController.comic.getSelected().equals(mainController.comic.getLeftCharacter())){
+      mainController.bottomLeftIV.setImage(null);
+      mainController.comic.setLeftCharacter(null);
+      mainController.centreLeft.setImage(null);
+      mainController.leftTextField.clear();
+      mainController.leftTextField.setVisible(false);
+    }
+    else{
+      mainController.bottomRightIV.setImage(null);
+      mainController.comic.setRightCharacter(null);
+      mainController.centreRight.setImage(null);
+      mainController.rightTextField.clear();
+      mainController.rightTextField.setVisible(false);
+    }
+    mainController.selectedBorder.setVisible(false);
+    mainController.comic.setSelected(null);
+    mainController.comicSelection = null;
+    mainController.comicCharacterSelection = null;
+    switchButtonState(false);
+  }
+
+}
