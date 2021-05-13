907d8c6d00141c651d38e52662041c3384b5dedd
Dáire Fagan <dairefagan@gmail.com>
2021-05-13 16:16:21 +0100

    Remove character & background count from test

--- src/main/java/app/test/TestImageLists.java
+++ src/main/java/app/test/TestImageLists.java
@@ -26,17 +26,10 @@
 //    // check that characterImages is not null
 //    assertNotNull(characterImages);
 //
-//    // check number of character images
-//    assertTrue(characterImages.size() == 32);
-//
 //    // check that imageList is not null
 //    assertNotNull(backgroundImages);
-//
-//    // check the number of background images
-//    assertTrue(backgroundImages.size() == 16);
 //
 //    // check that object equals
 //    assertEquals(imageLists, imageLists);
 //  }
-//
 //}
