24218dd690e545f3e084c0ce4b8aba463a6c61d0
Dáire Fagan <dairefagan@gmail.com>
2021-05-13 16:22:20 +0100

    Uncomment TestImageLists as working cross platform

--- src/main/java/app/test/TestImageLists.java
+++ src/main/java/app/test/TestImageLists.java
@@ -1,35 +1,35 @@
-//package app.test;
-//
-//import app.ImageLists;
-//import java.io.IOException;
-//import java.util.List;
-//import javafx.scene.image.Image;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-//public class TestImageLists {
-//
-//  @Test
-//  public void testImageLists() throws IOException {
-//    // test data
-//    ImageLists imageLists = new ImageLists();
-//
-//    List<Image> characterImages = imageLists.getCharacterImages();
-//    List<Image> backgroundImages = imageLists.getBackgroundImages();
-//
-//    // check that imageList is not null
-//    assertNotNull(imageLists);
-//
-//    // check imageLists is instance of imageLists
-//    assertTrue("test", imageLists instanceof ImageLists);
-//
-//    // check that characterImages is not null
-//    assertNotNull(characterImages);
-//
-//    // check that imageList is not null
-//    assertNotNull(backgroundImages);
-//
-//    // check that object equals
-//    assertEquals(imageLists, imageLists);
-//  }
-//}
+package app.test;
+
+import app.ImageLists;
+import java.io.IOException;
+import java.util.List;
+import javafx.scene.image.Image;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestImageLists {
+
+  @Test
+  public void testImageLists() throws IOException {
+    // test data
+    ImageLists imageLists = new ImageLists();
+
+    List<Image> characterImages = imageLists.getCharacterImages();
+    List<Image> backgroundImages = imageLists.getBackgroundImages();
+
+    // check that imageList is not null
+    assertNotNull(imageLists);
+
+    // check imageLists is instance of imageLists
+    assertTrue("test", imageLists instanceof ImageLists);
+
+    // check that characterImages is not null
+    assertNotNull(characterImages);
+
+    // check that imageList is not null
+    assertNotNull(backgroundImages);
+
+    // check that object equals
+    assertEquals(imageLists, imageLists);
+  }
+}
